20c0fbab0bcf6d978b82856876b31dcca6edd090
Andrés Briseño <75357780+androx999@users.noreply.github.com>
2022-11-26 20:21:05 -0600

    Evidencia 5 Manejo de Excepciones

--- Ev_4/src/Run.java
+++ Ev_4/src/Run.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Run {
@@ -13,16 +14,22 @@ public class Run {
 		
 		ArrayList<Empleado> empleados = new ArrayList<Empleado>();
 		
+		EdadException edE = new EdadException();
+		
 		do {
+			try {
 			//Desea registrar un usuario?
 			salir = true;
-		
 			System.out.println("Desea ingresar empleado(0) o programador(1)?");
 			if(number.nextInt()==1) {
 				//Ingresara programador
 				Programador progAux = new Programador();
 				System.out.println("Indique el nombre:");
 				progAux.setNombre(text.nextLine());
+				
+				System.out.println("Indique la edad: ");
+				progAux.setEdad(number.nextInt()); 
+				edE.validarEdad(progAux.getEdad());
 				
 				empleados.add(progAux);
 				
@@ -32,6 +39,10 @@ public class Run {
 				System.out.println("Indique el nombre: ");
 				empAux.setNombre(text.nextLine());
 				
+				System.out.println("Indique la edad: ");
+				empAux.setEdad(number.nextInt()); 
+				edE.validarEdad(empAux.getEdad());
+				
 				empleados.add(empAux);
 			}
 			
@@ -40,12 +51,21 @@ public class Run {
 				salir = true;
 			}else {
 				salir = false;
+			}	
+			}catch(InputMismatchException ex) {
+				System.out.println("Tome la decision correcta");
+				ex.printStackTrace();
+				salir = false;
+			} catch(EdadException ex) {
+				salir = false;
 			}
+			
 		}while(salir);
 		
 		for(int i = 0; i<empleados.size();i++) {
 			System.out.println("\nEmpleado: "+i);
 			System.out.println(empleados.get(i).getNombre());
 		}
-	}
+  }
 }
+
